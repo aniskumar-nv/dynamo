cf814f3f04ef298d2395c735a684df5f61ce1400
Qi Wang <qiwa@nvidia.com>
2026-06-30 13:34:26 -0700

    chore(multimodal): SPDX year 2026 on new files (drop 2025-2026)

--- components/src/dynamo/vllm/multimodal_utils/threaded_micro_batcher.py
+++ components/src/dynamo/vllm/multimodal_utils/threaded_micro_batcher.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 """Coalesce concurrent async calls into batched calls of a blocking fn on one thread.
